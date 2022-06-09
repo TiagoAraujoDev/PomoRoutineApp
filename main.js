3df8ee1e770275c05e28c427e18ce5f4f202aee2
TiagoAraujoDev <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-06-08 21:39:40 -0300

    timer feature complete

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const sessionBtn = document.getElementById('sessionSetting');
 const breakBtn = document.getElementById('breakSetting');
 const longBreakBtn = document.getElementById('longBreakSetting');
+const quantitySessionsBtn = document.getElementById('quantitySession');
 const sessionModal = document.getElementById('sessionModal');
 const breakModal = document.getElementById('breakModal');
 const longBreakModal = document.getElementById('longBreakModal');
+const quantitySessionsModal = document.getElementById('quantitySessionsModal');
 const closeModalBtn = document.querySelectorAll('#closeModalbtn');
 const minSessionInput = document.getElementById('minSession');
 const minBreakInput = document.getElementById('minBreak');
 const minLongBreakInput = document.getElementById('minLongBreak');
+const quantitySessionsInput = document.getElementById('userSessionsQuantity');
 const userSaveInputBtn = document.querySelectorAll('.saveTimeBtn');
 const startBtn = document.getElementById('startBtn');
 const pauseBtn = document.getElementById('pauseBtn');
@@ -20,35 +23,46 @@ const secText = document.getElementById('secText');
 let timeSession = parseInt(minSessionInput.value) * 60000;
 let timeBreak = parseInt(minBreakInput.value) * 60000;
 let timeLongBreak = parseInt(minLongBreakInput.value) * 60000;
+let userSessionsQuantity = quantitySessionsInput.value;
 
 minText.innerText = minSessionInput.value;
 
 let sessionCounter = 1;
-const userSessionsQuantity = 3;
-
 let interval;
-
-// const sound = new Audio('sound.mp3');
 
 function timerControl() {
   if (timeSession !== 0) {
-    if (timeSession === 1000 && sessionCounter < userSessionsQuantity) {
+    if (timeSession === 1000) {
+      playAlarm();
+      clearInterval(interval);
+      startBtn.classList.remove('disabled');
+    }
+    if (sessionCounter < userSessionsQuantity) {
       timeBreak = parseFloat(minBreakInput.value) * 60000;
       sessionCounter++;
     }
+    console.log('s');
     timeLongBreak = parseInt(minLongBreakInput.value) * 60000;
     countdownSession();
   } else if (timeSession === 0 && timeBreak !== 0) {
     if (timeBreak === 1000) {
+      playAlarm();
+      clearInterval(interval);
+      startBtn.classList.remove('disabled');
       timeSession = parseInt(minSessionInput.value) * 60000;
     }
+    console.log('b');
     countdownBreak();
   } else if (sessionCounter === userSessionsQuantity) {
     if (timeLongBreak === 1000) {
+      playAlarm();
+      clearInterval(interval);
+      startBtn.classList.remove('disabled');
       timeSession = parseInt(minSessionInput.value) * 60000;
       timeBreak = parseInt(minBreakInput.value) * 60000;
       sessionCounter = 1;
     }
+    console.log('lb');
     countdownLongBreak();
   }
 }
@@ -61,10 +75,11 @@ function countdownSession() {
   secText.innerText = format(seconds);
 
   timeSession -= 1000;
-  
+
   minText.innerText = format(minutes);
   secText.innerText = format(seconds);
 }
+
 function countdownBreak() {
   let minutes = Math.floor(timeBreak / 60000);
   let seconds = ((timeBreak % 60000) * 1000) / 1000000;
@@ -77,10 +92,11 @@ function countdownBreak() {
   minText.innerText = format(minutes);
   secText.innerText = format(seconds);
 }
+
 function countdownLongBreak() {
   let minutes = Math.floor(timeLongBreak / 60000);
   let seconds = ((timeLongBreak % 60000) * 1000) / 1000000;
-  
+
   minText.innerText = format(minutes);
   secText.innerText = format(seconds);
 
@@ -90,90 +106,17 @@ function countdownLongBreak() {
   secText.innerText = format(seconds);
 }
 
-/* function countdownTimer() {
-  switch (sessionControl()) {
-    case 'session':
-      minutes = Math.floor(timeSession / 60000);
-      seconds = ((timeSession % 60000) * 1000) / 1000000;
-
-      minText.innerText = format(minutes);
-      secText.innerText = format(seconds);
-
-      timeSession -= 1000;
-      break;
-    case 'break':
-      minutes = Math.floor(timeBreak / 60000);
-      seconds = ((timeBreak % 60000) * 1000) / 1000000;
-
-      minText.innerText = format(minutes);
-      secText.innerText = format(seconds);
-
-      timeBreak -= 1000;
-      break;
-    case 'longBreak':
-      minutes = Math.floor(timeLongBreak / 60000);
-      seconds = ((timeLongBreak % 60000) * 1000) / 1000000;
-
-      minText.innerText = format(minutes);
-      secText.innerText = format(seconds);
-
-      timeLongBreak -= 1000;
-      break;
-  }
+function playAlarm() {
+  let audio = document.createElement('audio');
+  audio.src = './assets/alarm.wav';
+  audio.play();
 }
- */
-/* function countdownTimer() {
-  if (breakCounter <= 2) {
-    if (timeSession != 0) {
-      let minutes = Math.floor(timeSession / 60000);
-      let seconds = ((timeSession % 60000) * 1000) / 1000000;
-
-      minText.innerText = format(minutes);
-      secText.innerText = format(seconds);
-
-      timeSession -= 1000;
-    } else if (timeSession == 0) {
-      if (timeSession == 0) {
-        clearInterval(interval);
-      }
-      let minutes = Math.floor(timeBreak / 60000);
-      let seconds = ((timeBreak % 60000) * 1000) / 1000000;
-      const minText = document.getElementById('minText');
-      const secText = document.getElementById('secText');
-
-      minText.innerText = format(minutes);
-      secText.innerText = format(seconds);
-
-      timeBreak -= 1000;
-      if (timeBreak == 0) {
-        timeBreak = parseInt(minBreakInput.value) * 60000;
-        breakCounter++;
-      }
-    }
-  } else {
-    let minutes = Math.floor(timeLongBreak / 60000);
-    let seconds = ((timeLongBreak % 60000) * 1000) / 1000000;
-    const minText = document.getElementById('minText');
-    const secText = document.getElementById('secText');
-
-    minText.innerText = format(minutes);
-    secText.innerText = format(seconds);
-
-    timeLongBreak -= 1000;
-    if (timeLongBreak == 0) {
-      breakCounter = 0;
-      timeSession = parseInt(minSessionInput.value) * 60000;
-      timeBreak = parseInt(minBreakInput.value) * 60000;
-      timeLongBreak = parseInt(minLongBreakInput.value) * 60000;
-    }
-  }
-} */
 
 function format(number) {
   return (number < 10 ? '0' : '') + number;
 }
 
-/* =========== Interactions Events ================ */
+/* =========== EventListeners ================ */
 
 sessionBtn.addEventListener('click', () => {
   sessionModal.classList.add('d-block');
@@ -207,17 +150,30 @@ longBreakBtn.addEventListener('click', () => {
   });
 });
 
+quantitySessionsBtn.addEventListener('click', () => {
+  quantitySessionsModal.classList.add('d-block');
+  closeModalBtn[3].addEventListener('click', () => {
+    quantitySessionsModal.classList.remove('d-block');
+  });
+  userSaveInputBtn[3].addEventListener('click', () => {
+    userSessionsQuantity = quantitySessionsInput.value;
+  });
+});
+
 startBtn.addEventListener('click', () => {
   timerControl();
-  interval = setInterval(timerControl, 1000);
+  interval = setInterval(timerControl, 150);
+  startBtn.classList.add('disabled');
 });
 
 pauseBtn.addEventListener('click', () => {
   clearInterval(interval);
+  startBtn.classList.remove('disabled');
 });
 
 resetBtn.addEventListener('click', () => {
   clearInterval(interval);
+  startBtn.classList.remove('disabled');
   timeSession = parseInt(minSessionInput.value) * 60000;
   timeBreak = parseInt(minBreakInput.value) * 60000;
   timeLongBreak = parseInt(minLongBreakInput.value) * 60000;
