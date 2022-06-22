caffc80267b9655752e8c65503dacdd6ae5fd5bf
TiagoAraujoDev <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-06-21 22:25:58 -0300

    create: notes system

--- pages/notes/index.js
+++ pages/notes/index.js
@@ -1,9 +1,12 @@
 const addNotesBtn = document.getElementById("addNotesbtn");
 const noteModal = document.getElementById("noteModal");
-const closeEditModalBtn = document.getElementById("closeEditModalBtn");
+const closeNoteModalBtn = document.getElementById("closeNoteModalBtn");
 const addNoteModalBtn = document.getElementById("addNoteModalBtn");
 const titleInput = document.getElementById("noteTitleInput");
 const bodyInput = document.getElementById("noteBodyInput");
+const closeEditModalBtn = document.getElementById("closeEditModalBtn");
+const editNoteModalBtn = document.getElementById("editNoteModalBtn");
+const editNoteModal = document.getElementById("editNoteModal");
 
 const notes = [];
 
@@ -13,10 +16,10 @@ function createNote(title, body) {
   note.classList.add("col-1", "card", "myNote");
   note.innerHTML = `
     <div class="card-header bg-dark d-flex justify-content-between">
-      <span class="text-white">${title}</span>
+      <span id="title"  class="text-white">${title}</span>
       <div class="">
-        <i class="bx bxs-edit text-white"></i>
-        <i class="bx bx-trash text-white"></i>
+        <i id="editNoteBtn" class="bx bxs-edit text-white" onclick="showEditModal(this)"></i>
+        <i id="removeNoteBtn" class="bx bx-trash text-white" onclick="removeNote(this)"></i>
       </div>
     </div>
     <div class="card-body">
@@ -24,6 +27,37 @@ function createNote(title, body) {
     </div>
   `;
   noteContainer.appendChild(note);
+}
+
+function showEditModal(htmlElement) {
+  editNoteModal.classList.add("d-block");
+
+  editNoteModalBtn.addEventListener("click", () => {
+    editNote(htmlElement);
+  });
+}
+
+function editNote(htmlElement) {
+  // const note = htmlElement.parentNode.parentNode.parentNode;
+  const noteIndentifier =
+    htmlElement.parentNode.previousElementSibling.innerText;
+
+  const note = notes.find(note => note.title === noteIndentifier);
+
+  note.title = document.getElementById("editNoteTitleInput").value;
+  note.body = document.getElementById("editNoteBodyInput").value;
+
+  updateNotes();
+}
+
+function removeNote(htmlElement) {
+  const noteIndentifier =
+    htmlElement.parentNode.previousElementSibling.innerText;
+
+  const noteIndex = notes.findIndex(note => note.title === noteIndentifier);
+  notes.splice(noteIndex, 1);
+
+  updateNotes();
 }
 
 function setNote() {
@@ -51,11 +85,15 @@ function updateNotes() {
   notes.forEach(note => createNote(note.title, note.body));
 }
 
+closeEditModalBtn.addEventListener("click", () => {
+  editNoteModal.classList.remove("d-block");
+});
+
 addNotesBtn.addEventListener("click", () => {
   noteModal.classList.add("d-block");
 });
 
-closeEditModalBtn.addEventListener("click", () => {
+closeNoteModalBtn.addEventListener("click", () => {
   noteModal.classList.remove("d-block");
 });
 
